84ba8e8f5afca9dd0d55895daf0c1f1a6ea56d17
Nieblas <nieblasleonel@gmail.com>
2024-03-03 18:10:32 -0600

    feat: reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,42 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+  console.log(counter);
+
+  const incrementHandler = () =>{
+    dispatch({type: 'INCREMENT'});
+  }
+  const increaseHandler = () =>{
+    dispatch({type: 'INCREASE', amount: 5 });
+  }
+  const decrementHandler = () =>{
+    dispatch({type: 'DECREMENT'});
+  }
+  const resetHandler = () => {
+    dispatch({type: 'RESET'});
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'TOGGLE'});
+  };
+
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={increaseHandler}>+5</button>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,42 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0}, action) => {
+
+const initialState = {
+    counter: 0 , 
+    showCounter: true
+}
+
+const counterReducer = (state = initialState, action) => {
 
     if(action.type==='INCREMENT'){
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter
+        };
+    }
+    if(action.type==='INCREASE'){
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         };
     }
     if(action.type==='DECREMENT'){
         if(state.counter > 0)
               return {
                 counter: state.counter - 1,
+                showCounter: state.showCounter
+            };
+    }
+    if(action.type==='RESET'){        
+              return {
+                counter: 0,
+                showCounter: state.showCounter
+            };
+    }
+    if(action.type==='TOGGLE'){        
+              return {
+                counter: state.counter,                
+                showCounter: !state.showCounter
             };
     }
 
